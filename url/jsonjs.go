a9be7a5419751488b3cb86a0859d5beec18bc20c
Ramesh VK <ramesh.vyaghrapuri@gmail.com>
2018-09-24 16:10:21 +0530

    Add url().json() support for fetching json

---
+++ url/jsonjs.go
@@ -0,0 +1,24 @@
+// Copyright (C) 2018 Ramesh Vyaghrapuri. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+// +build js,!jsreflect
+
+package url
+
+import (
+	"github.com/funnelorg/funnel/parse"
+	"github.com/funnelorg/funnel/run"
+	"honnef.co/go/js/xhr"
+)
+
+func (u URL) json(s run.Scope, args []parse.Node) interface{} {
+	req := xhr.NewRequest("GET", string(u))
+	req.Timeout = 1000 // one second, in milliseconds
+	req.ResponseType = xhr.JSON
+	err := req.Send(nil)
+	if err != nil {
+		return err
+	}
+	return req.Response.Interface()
+}
